ab12c598b229e37a53465fd20762992197a7a094
Prajwal B S <prajwaljhon@gmail.com>
2024-12-03 21:18:05 +0530

    Added Loading Gif...Not working Well Enough😎🧠✌

---
+++ Cart/static/js/scripts.js
@@ -0,0 +1,10 @@
+// preloader
+const preloader = document.querySelector(".preloader");
+
+// window.addEventListener("load", function () {
+//   preloader.classList.add("hide-preloader");
+// });
+console.log("HOIUHG")
+setTimeout(()=>{
+    preloader.classList.add("hide-preloader");
+},5000)
